a342ae0f31997a584433e338c2c8330bea2aedeb
Charan M <109715226+Charan-M22@users.noreply.github.com>
2024-08-06 08:19:39 +0530

    Final push
    Simply run on pull.

---
+++ server.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class server
+{
+    public static void main(String args[])
+    {
+        System.out.println("Enter your username: ");
+        String user = obj.nextLine();
+        System.out.println("Are u admin or user?")
+        String option = obj.nextLine();
+        System.out.println("Hello "+ user+ " u are a "+ option+"! Here are ur posts");
+        System.out.println("That brings us to the end of our demo thank you :)");
+    }
+}
